1b1ca050c8599a86348d02ca78242b76d819b055
cha <rose3470@gmail.com>
2026-03-07 19:51:56 +0900

    20260307

--- playlist_data.js
+++ playlist_data.js
@@ -1,28 +1,52 @@
 const playlists = {
     "enka": [
         {
-            "title": "이성애 - 단장의미아리고개",
-            "url": "music/enka/이성애 - 단장의미아리고개.mp3"
+            "title": "Four Seasons 'Autumn'-  Allegro (La Caccia)",
+            "url": "music/enka/Four Seasons 'Autumn'-  Allegro (La Caccia).mp3"
         },
         {
-            "title": "이성애 - 돌아와요부산항에",
-            "url": "music/enka/이성애 - 돌아와요부산항에.mp3"
+            "title": "Four Seasons 'Autumn'- Adagio",
+            "url": "music/enka/Four Seasons 'Autumn'- Adagio.mp3"
         },
         {
-            "title": "이성애 - 물방아도는내력",
-            "url": "music/enka/이성애 - 물방아도는내력.mp3"
+            "title": "Four Seasons 'Autumn'- Allegro",
+            "url": "music/enka/Four Seasons 'Autumn'- Allegro.mp3"
         },
         {
-            "title": "주현미 - 스물살때 어느밤",
-            "url": "music/enka/주현미 - 스물살때 어느밤.mp3"
+            "title": "Four Seasons 'Spring'- Allegro (danza pastorale)",
+            "url": "music/enka/Four Seasons 'Spring'- Allegro (danza pastorale).mp3"
         },
         {
-            "title": "주현미 - 정말 좋았네",
-            "url": "music/enka/주현미 - 정말 좋았네.mp3"
+            "title": "Four Seasons 'Spring'- Allegro",
+            "url": "music/enka/Four Seasons 'Spring'- Allegro.mp3"
         },
         {
-            "title": "주현미 - 정주지않으리",
-            "url": "music/enka/주현미 - 정주지않으리.mp3"
+            "title": "Four Seasons 'Spring'- Largo e pianissimo",
+            "url": "music/enka/Four Seasons 'Spring'- Largo e pianissimo.mp3"
+        },
+        {
+            "title": "Four Seasons 'Summer'-  Adagio, presto",
+            "url": "music/enka/Four Seasons 'Summer'-  Adagio, presto.mp3"
+        },
+        {
+            "title": "Four Seasons 'Summer'-  Presto (tempo impetuoso)",
+            "url": "music/enka/Four Seasons 'Summer'-  Presto (tempo impetuoso).mp3"
+        },
+        {
+            "title": "Four Seasons 'Summer'- Allegro non molto",
+            "url": "music/enka/Four Seasons 'Summer'- Allegro non molto.mp3"
+        },
+        {
+            "title": "Four Seasons 'Winter'- Allegro non molto",
+            "url": "music/enka/Four Seasons 'Winter'- Allegro non molto.mp3"
+        },
+        {
+            "title": "Four Seasons 'Winter'- Allegro",
+            "url": "music/enka/Four Seasons 'Winter'- Allegro.mp3"
+        },
+        {
+            "title": "Four Seasons 'Winter'- Largo",
+            "url": "music/enka/Four Seasons 'Winter'- Largo.mp3"
         }
     ],
     "jazz": [
@@ -49,12 +73,12 @@ const playlists = {
     ],
     "old": [
         {
-            "title": "Beyonce - TEXAS HOLD EM",
-            "url": "music/old/Beyonce - TEXAS HOLD EM.mp3"
+            "title": "Agnetha Fältskog - When You Really Loved Someone",
+            "url": "music/old/Agnetha Fältskog - When You Really Loved Someone.mp3"
         },
         {
-            "title": "Bobby Darin - Early In The Morning",
-            "url": "music/old/Bobby Darin - Early In The Morning.mp3"
+            "title": "Beyonce - TEXAS HOLD EM",
+            "url": "music/old/Beyonce - TEXAS HOLD EM.mp3"
         },
         {
             "title": "Daniel - Blackboard of My Heart",
@@ -73,10 +97,6 @@ const playlists = {
             "url": "music/old/Isla Grant - Only Yesterday.mp3"
         },
         {
-            "title": "Jonny Cash - Cotton Fields",
-            "url": "music/old/Jonny Cash - Cotton Fields.mp3"
-        },
-        {
             "title": "Mary Duff - Just Lovin' You",
             "url": "music/old/Mary Duff - Just Lovin' You.mp3"
         },
@@ -89,8 +109,8 @@ const playlists = {
             "url": "music/old/Mary Duff - Will You Walk With Me.mp3"
         },
         {
-            "title": "The Carpenters - Yesterday Once More",
-            "url": "music/old/The Carpenters - Yesterday Once More.mp3"
+            "title": "dave - The Happy Organ",
+            "url": "music/old/dave - The Happy Organ.flac"
         }
     ]
 };
