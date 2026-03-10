8e8fb921d32d142a4e769c73473cd82dd5bb03b9
cha <rose3470@gmail.com>
2026-03-10 15:18:04 +0900

    2026031066

---
+++ 11.js
@@ -0,0 +1,128 @@
+const playlists = {
+    "enka": [
+        {
+            "title": "Four Seasons 'Autumn'-  Allegro (La Caccia)",
+            "url": "music/enka/Four Seasons 'Autumn'-  Allegro (La Caccia).mp3"
+        },
+        {
+            "title": "Four Seasons 'Autumn'- Adagio",
+            "url": "music/enka/Four Seasons 'Autumn'- Adagio.mp3"
+        },
+        {
+            "title": "Four Seasons 'Autumn'- Allegro",
+            "url": "music/enka/Four Seasons 'Autumn'- Allegro.mp3"
+        },
+        {
+            "title": "Four Seasons 'Spring'- Allegro (danza)",
+            "url": "music/enka/Four Seasons 'Spring'- Allegro (danza).mp3"
+        },
+        {
+            "title": "Four Seasons 'Spring'- Allegro",
+            "url": "music/enka/Four Seasons 'Spring'- Allegro.mp3"
+        },
+        {
+            "title": "Four Seasons 'Spring'- Largo e pianissimo",
+            "url": "music/enka/Four Seasons 'Spring'- Largo e pianissimo.mp3"
+        },
+        {
+            "title": "Four Seasons 'Summer'-  Adagio, presto",
+            "url": "music/enka/Four Seasons 'Summer'-  Adagio, presto.mp3"
+        },
+        {
+            "title": "Four Seasons 'Summer'-  Presto (tempo o)",
+            "url": "music/enka/Four Seasons 'Summer'-  Presto (tempo o).mp3"
+        },
+        {
+            "title": "Four Seasons 'Summer'- Allegro non molto",
+            "url": "music/enka/Four Seasons 'Summer'- Allegro non molto.mp3"
+        },
+        {
+            "title": "Four Seasons 'Winter'- Allegro non molto",
+            "url": "music/enka/Four Seasons 'Winter'- Allegro non molto.mp3"
+        },
+        {
+            "title": "Four Seasons 'Winter'- Allegro",
+            "url": "music/enka/Four Seasons 'Winter'- Allegro.mp3"
+        },
+        {
+            "title": "Four Seasons 'Winter'- Largo",
+            "url": "music/enka/Four Seasons 'Winter'- Largo.mp3"
+        }
+    ],
+    "jazz": [
+        {
+            "title": "금잔디 - 고장난벽시계",
+            "url": "music/jazz/금잔디 - 고장난벽시계.mp3"
+        },
+        {
+            "title": "금잔디 - 남자라는이유로",
+            "url": "music/jazz/금잔디 - 남자라는이유로.mp3"
+        },
+        {
+            "title": "금잔디 - 네박자",
+            "url": "music/jazz/금잔디 - 네박자.mp3"
+        },
+        {
+            "title": "금잔디 - 안동역에서",
+            "url": "music/jazz/금잔디 - 안동역에서.mp3"
+        },
+        {
+            "title": "금잔디 - 일편단심",
+            "url": "music/jazz/금잔디 - 일편단심.mp3"
+        }
+    ],
+    "old": [
+        {
+            "title": "Beyonce - TEXAS HOLD EM",
+            "url": "music/old/Beyonce - TEXAS HOLD EM.mp3"
+        },
+        {
+            "title": "Daniel - Blackboard of My Heart",
+            "url": "music/old/Daniel - Blackboard of My Heart.mp3"
+        },
+        {
+            "title": "Daniel - Pretty Little Girl From Omagh",
+            "url": "music/old/Daniel - Pretty Little Girl From Omagh.mp3"
+        },
+        {
+            "title": "Green Green Grass Of Home",
+            "url": "music/old/Green Green Grass Of Home.mp3"
+        },
+        {
+            "title": "Isla Grant - Every Moment Of Every Hour",
+            "url": "music/old/Isla Grant - Every Moment Of Every Hour.mp3"
+        },
+        {
+            "title": "Isla Grant - Only Yesterday",
+            "url": "music/old/Isla Grant - Only Yesterday.mp3"
+        },
+        {
+            "title": "Jonny Cash - Cotton Fields",
+            "url": "music/old/Jonny Cash - Cotton Fields.mp3"
+        },
+        {
+            "title": "Mary Duff - Just Lovin' You",
+            "url": "music/old/Mary Duff - Just Lovin' You.mp3"
+        },
+        {
+            "title": "Mary Duff - Power Of Love",
+            "url": "music/old/Mary Duff - Power Of Love.mp3"
+        },
+        {
+            "title": "Mary Duff - Will You Walk With Me",
+            "url": "music/old/Mary Duff - Will You Walk With Me.mp3"
+        },
+        {
+            "title": "The Carpenters - Yesterday Once More",
+            "url": "music/old/The Carpenters - Yesterday Once More.mp3"
+        },
+        {
+            "title": "Tom Jones - Delilah",
+            "url": "music/old/Tom Jones - Delilah.mp3"
+        },
+        {
+            "title": "dave - The Happy Organ",
+            "url": "music/old/dave - The Happy Organ.flac"
+        }
+    ]
+};
